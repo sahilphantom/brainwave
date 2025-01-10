f9e68fd8a8d3acc34fb3039b71f75623ea5b7327
sahilphantom <sahilmahmood188@gmail.com>
2025-01-10 20:12:27 +0500

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { Link } from 'react-router-dom';
 import { brainwave } from "../assets";
-import { navigation } from "../constants";
+// import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
-import { useState } from "react";
+import { useState } from "react"; 
 
 const Header = () => {
   const pathname = useLocation();
@@ -45,26 +45,24 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed p-[1.9rem] top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-              >
-                {item.title}
-              </a>
-            ))}
+            {/* Navigation Links */}
+            <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            <Link to="/features"  className=' button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+              FEATURES
+            </Link>
+            <Link to="/pricing"className=' button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+              PRICING
+            </Link>
+            <Link to="/how-to-use"className=' button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+              HOW TO USE
+            </Link>
+            <Link to="/roadmap"className=' button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+              ROADMAP
+            </Link>
           </div>
+         
           
 
           <HamburgerMenu />
